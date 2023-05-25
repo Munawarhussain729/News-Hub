0710b386027b00abd3123b53cf8180a92f27a555
Munawar Hussain <munawarhussa.7@gmail.com>
2023-05-25 13:24:44 +0500

    Spinner: Add spinner on each page

--- src/app/dashboard/sports/page.js
+++ src/app/dashboard/sports/page.js
@@ -1,20 +1,23 @@
 'use client'
 import { API_KEY } from "@/app/APIKEY";
+import Loader from "@/app/Components/Loader";
 import MainHeadline from "@/app/Components/MainHeadline";
 import NewsCard from "@/app/Components/NewsCard";
 import { insertArticles } from "@/app/store/newsSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Sports() {
 
   const dispatch = useDispatch()
+  const [showLoader, setShowLoader] = useState(true)
 
   useEffect(() => {
     fetch(`https://newsapi.org/v2/top-headlines?category=sports&language=en&pageSize=20&apiKey=${API_KEY}`)
       .then(response => response.json())
       .then(data => {
-        dispatch(insertArticles(data?.articles))
+        dispatch(insertArticles(data?.articles));
+        setShowLoader(false);
       })
       .catch(error => {
         // Handle any errors here
@@ -25,15 +28,18 @@ export default function Sports() {
   const selector = useSelector((state) => state)
   const newsData = selector?.newSlice?.newsArticles
   return (
-    <div>
-      <MainHeadline />
-      <div className="flex flex-wrap justify-center">
-        {newsData?.map((item, index) => {
-          return (
-            <NewsCard key={index} newsData={item} />
-          )
-        })}
-      </div>
-    </div>
+    <>
+      {showLoader ? <Loader /> :
+        <div>
+          <MainHeadline />
+          <div className="flex flex-wrap justify-center">
+            {newsData?.map((item, index) => {
+              return (
+                <NewsCard key={index} newsData={item} />
+              )
+            })}
+          </div>
+        </div>}
+    </>
   )
 }
